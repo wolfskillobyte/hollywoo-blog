4f71d7f749cf13dab2f9a03a833e241e4cbab041
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-12-03 14:51:08 -0500

    create user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -55,4 +55,20 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// CREATE user
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then((dbUserData) => {
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
