9435708f23ea80975d519cc33d90d880a8611946
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-12-03 14:52:15 -0500

    update user info

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -71,4 +71,26 @@ router.post('/', (req, res) => {
     });
 });
 
+// UPDATE user info
+router.put('/:id', (req, res) => {
+  // pass in req.body instead to only update what's passed through
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
